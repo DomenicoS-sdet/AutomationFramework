6d024f46a7bcfa3c9c89c9f1ae34d37551619d95
kamel <ziosamma@gmail.com>
2019-07-06 20:53:51 +0200

    Embending Logger inside the BeforeAfterSteps file

--- FeatureFiles/BeforeAfterSteps.cs
+++ FeatureFiles/BeforeAfterSteps.cs
@@ -5,22 +5,80 @@ using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 using Drivers;
+using Report.LogContainers;
 
 namespace FeatureFiles
 {
     [Binding]
     public class BeforeAfterSteps
     {
-        [BeforeScenario]
+        private TestRunLogger _logger;
+        private FeatureLogger _featureLogger;
+        private ScenarioLogger _scenarioLogger;
+
+        [BeforeTestRun]
+        public void BeforeTestRun()
+        {
+            _logger = new TestRunLogger();
+        }
+
+        [BeforeFeature]
+        public void BeforeFeature()
+        {
+            _featureLogger = new FeatureLogger();
+            _featureLogger.FeatureTitle = FeatureContext.Current.FeatureInfo.Title;
+            _logger.AddFeature(_featureLogger);
+
+        }
+
+        [BeforeScenario(Order =1)]
+        public void BeforeScenario()
+        {
+            _scenarioLogger = new ScenarioLogger();
+            _scenarioLogger.ScenarioTitle = ScenarioContext.Current.ScenarioInfo.Title;
+            _featureLogger.AddScenario(_scenarioLogger);
+        }
+
+        [BeforeScenario(Order =2)]
         public void StartBrowser()
         {
             Browser.Open();
         }
 
-        [AfterScenario]
+        [BeforeStep]
+        public void BeforeStep()
+        {
+
+        }
+
+        [AfterStep]
+        public void AfterStep()
+        {
+
+        }
+
+        [AfterScenario(Order = 1)]
+        public void AfterScenario()
+        {
+
+        }
+
+        [AfterScenario(Order =2)]
         public void CloseBrowser()
         {
             Browser.Close();
+        }
+
+        [AfterFeature]
+        public void AfterFeature()
+        {
+
+        }
+
+        [AfterTestRun]
+        public void AfterTestRun()
+        {
+
         }
     }
 }
--- Report/LogContainers/ScenarioLogger.cs
+++ Report/LogContainers/ScenarioLogger.cs
@@ -9,6 +9,7 @@ namespace Report.LogContainers
         private List<StepLogger> _steps = new List<StepLogger>();
         public string ScenarioTitle { get; set; }
         public string ScenarioResult { get; set; }
+        public string ScenarioDuration { get; set; }
 
         public void AddStep(StepLogger step) => _steps.Add(step);
         public List<StepLogger> GetStepsList() => _steps; 
