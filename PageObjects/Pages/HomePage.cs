af6cb8b4ac9859e51abf27d509cdd596656d5675
kamel <kamel@DESKTOP-ID5FVM1>
2019-07-27 18:47:17 +0200

    Updated some id to locate the elements on the page to change the number of travellers

--- PageObjects/Pages/HomePage.cs
+++ PageObjects/Pages/HomePage.cs
@@ -19,9 +19,9 @@ namespace PageObjects.Pages
         private ElementScope CitySearch => _browserSession.FindXPath("//span[text()='Search by Hotel or City Name']/parent::a/parent::div//input");
         private ElementScope CheckIn => _browserSession.FindXPath("//input[@placeholder=\"Check in\" and @name='checkin']");
         private ElementScope CheckOut => _browserSession.FindXPath("//input[@placeholder=\"Check out\" and @name='checkout']");
-        private ElementScope TravellersInput => _browserSession.FindId("travellersInput");
-        private ElementScope AdultInput => _browserSession.FindId("adultInput");
-        private ElementScope ChildInput => _browserSession.FindId("childInput");
+        private ElementScope TravellersInput => _browserSession.FindId("htravellersInput");
+        private ElementScope AdultInput => _browserSession.FindId("hadultInput");
+        private ElementScope ChildInput => _browserSession.FindId("hchildInput");
 
         public bool IsHomePageReady()
         {
@@ -45,22 +45,22 @@ namespace PageObjects.Pages
             {
                 if(int.Parse(AdultInput.GetAttribute("value")) < int.Parse(hotelSearch.Adults))
                 {
-                    _browserSession.FindId("adultPlusBtn").Click();
+                    _browserSession.FindId("hadultPlusBtn").Click();
                 }
                 else
                 {
-                    _browserSession.FindId("adultMinusBtn").Click();
+                    _browserSession.FindId("hadultMinusBtn").Click();
                 }
             }
             while (!ChildInput.GetAttribute("value").Equals(hotelSearch.Childs))
             {
                 if (int.Parse(ChildInput.GetAttribute("value")) < int.Parse(hotelSearch.Childs))
                 {
-                    _browserSession.FindId("childPlusBtn").Click();
+                    _browserSession.FindId("hchildPlusBtn").Click();
                 }
                 else
                 {
-                    _browserSession.FindId("childMinusBtn").Click();
+                    _browserSession.FindId("hchildMinusBtn").Click();
                 }
             }
 
